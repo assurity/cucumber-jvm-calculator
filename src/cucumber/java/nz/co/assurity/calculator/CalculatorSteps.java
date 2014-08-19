b2980491ef8e053c6e4b843496f929a109e857c2
Nigel Charman <nigel.charman.nz@gmail.com>
2014-08-19 16:37:43 +1200

    Initial version

---
+++ src/cucumber/java/nz/co/assurity/calculator/CalculatorSteps.java
@@ -0,0 +1,23 @@
+package nz.co.assurity.calculator;
+
+import static org.junit.Assert.*;
+
+import static org.hamcrest.CoreMatchers.*;
+
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class CalculatorSteps {
+	private Calculator calculator = new Calculator();
+	private int result;
+
+	@When("^I calculate (\\d+) \\+ (\\d+)$")
+	public void i_calculate(int operand1, int operand2) throws Throwable {
+	    result = calculator.add(operand1, operand2);
+	}
+
+	@Then("^the answer is (\\d+)$")
+	public void the_answer_is(int expected) throws Throwable {
+		assertThat(result, equalTo(expected));
+	}
+}
---
+++ src/main/java/nz/co/assurity/calculator/Calculator.java
@@ -0,0 +1,8 @@
+package nz.co.assurity.calculator;
+
+public class Calculator {
+
+	public int add(int operand1, int operand2) {
+		return operand1 + operand2;
+	}
+}
