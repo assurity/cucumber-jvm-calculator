4ed619f2a52aeb1dc0b642afa90a4145574b398a
Nigel Charman <nigel.charman.nz@gmail.com>
2021-11-06 13:07:14 +1300

    Revert "Implement division"
    This reverts commit 35fa3a7bceadce16dfe5becf1b902e8c0cf90b8e.

--- src/main/java/nz/co/assurity/calculator/Calculator.java
+++ src/main/java/nz/co/assurity/calculator/Calculator.java
@@ -5,8 +5,4 @@ public class Calculator {
 	public int add(int operand1, int operand2) {
 		return operand1 + operand2;
 	}
-
-	public int divide(int operand1, int operand2) {
-		return operand1 / operand2;
-	}
 }
